2e644396b704dd9752e05ba474f686c7642e9cab
patel ishani <patelishani665@gmail.com>
2024-08-30 16:04:02 +0530

    commit msg

--- frontend/src/components/recruiter/recruiter-profile/recruiterProfile.js
+++ frontend/src/components/recruiter/recruiter-profile/recruiterProfile.js
@@ -1,13 +1,64 @@
-import React from 'react'
-import RecruiterNavbar from '../recruiterNavbar'
+import React from 'react';
+import RecruiterNavbar from '../recruiterNavbar';
+import './recruiterProfile.css'
+const RecruiterProfile = () => {
+  const recruiterData = {
+    firstName: 'ishani',
+    middleName: 'hiteshkumar',
+    lastName: 'patel',
+    email: 'ishani@example.com',
+    mobileNumber: '1234567890',
+    dateOfBirth: '2005-14-02',
+    username: 'ishanipatel',
+    designation: 'Recruiter',
+    companyName: 'charusat',
+  };
 
-const recruiterProfile = () => {
   return (
-    <div className='bg-gray-100 min-h-screen'>
+    
+    <div className="recruiter-profile-container">
       <RecruiterNavbar />
-      Recruiter profile
+      <div className="profile-details">
+        <h2>Recruiter Profile</h2>
+        <p>
+          <span className="profile-details-label">First Name:</span>
+          <span className="profile-details-value">{recruiterData.firstName}</span>
+        </p>
+        <p>
+          <span className="profile-details-label">Middle Name:</span>
+          <span className="profile-details-value">{recruiterData.middleName}</span>
+        </p>
+        <p>
+          <span className="profile-details-label">Last Name:</span>
+          <span className="profile-details-value">{recruiterData.lastName}</span>
+        </p>
+        <p>
+          <span className="profile-details-label">Email:</span>
+          <span className="profile-details-value">{recruiterData.email}</span>
+        </p>
+        <p>
+          <span className="profile-details-label">Mobile Number:</span>
+          <span className="profile-details-value">{recruiterData.mobileNumber}</span>
+        </p>
+        <p>
+          <span className="profile-details-label">Date of Birth:</span>
+          <span className="profile-details-value">{recruiterData.dateOfBirth}</span>
+        </p>
+        <p>
+          <span className="profile-details-label">Username:</span>
+          <span className="profile-details-value">{recruiterData.username}</span>
+        </p>
+        <p>
+          <span className="profile-details-label">Designation:</span>
+          <span className="profile-details-value">{recruiterData.designation}</span>
+        </p>
+        <p>
+          <span className="profile-details-label">Company Name:</span>
+          <span className="profile-details-value">{recruiterData.companyName}</span>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default recruiterProfile
+export default RecruiterProfile;
